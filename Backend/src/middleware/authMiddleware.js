08fde9d60fd4567aa1aef766b622c26c5545376d
Aayush1Adk <aayush.adhikari2076@gmail.com>
2026-08-03 21:27:48 +0545

    add account controller and authentication middleware for user validation

---
+++ Backend/src/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const userModel = require('../models/user.model.js');
+const jwt = require('jsonwebtoken');
+
+
+async function authMiddleware(req, res, next) {
+
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await userModel.findById(decoded.userId);
+        req.user = user; // Attach the user object to the request for further use in the route handlers
+        
+        next();
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid token." });
+    }
+}
+
+module.exports = {authMiddleware};
