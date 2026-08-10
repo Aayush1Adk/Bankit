0c4c2d23a5724a7eb7623451dab3a5d2a5f13c8e
Aayush1Adk <aayush.adhikari2076@gmail.com>
2026-08-10 20:45:54 +0545

    feat: add authSystemUserMiddleware for system user access control

--- Backend/src/middleware/authMiddleware.js
+++ Backend/src/middleware/authMiddleware.js
@@ -22,4 +22,32 @@ async function authMiddleware(req, res, next) {
     }
 }
 
-module.exports = {authMiddleware};
+async function authSystemUserMiddleware( req, res, next) {
+
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({message:"Access denied. No token provided."});
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);  
+
+        const user = await userModel.findById(decoded.userId).select("+systemUser")
+
+        if(!user.systemUser){
+            return res.status(403).json({message:"Access denied. User is not a system user."});
+        }
+
+        req.user = user; // Attach the user object to the request for further use in the route handlers
+        
+        return next();
+    }
+    catch(err){
+        return res.status(401).json({message:"Invalid token."});
+    }
+}
+
+
+
+module.exports = {authMiddleware, authSystemUserMiddleware};
