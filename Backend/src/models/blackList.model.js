58ceb0226c40b43745e1ca2263c989578ddebe3f
Aayush1Adk <aayush.adhikari2076@gmail.com>
2026-08-15 11:32:56 +0545

    feat: enhance createTransaction function with improved response messages and error handling; add blackListToken model for token management

---
+++ Backend/src/models/blackList.model.js
@@ -0,0 +1,19 @@
+const mongoose = required("mongoose");
+
+const blackListToken = new mongoose.Schema({
+    token:{
+        type:String,
+        required:[true, "Token is required"],
+        unique:[true, "Token already exists"]
+    }
+},{
+    timestamps:true
+});
+
+blackListToken.index({createdAt: 1},
+    {expireAfterSeconds : 60*60*24*3
+});
+
+const TokenBlackList = mongoose.model("TokenBlackList", blackListToken);
+
+module.exports = TokenBlackList;
